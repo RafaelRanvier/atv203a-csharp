1fe6d333a5b1f5101f5549f56d8274396cee8e0e
RafaelRanvier <rafaelranvier@bol.com.br>
2023-02-09 01:06:46 -0300

    sem interface

---
+++ Atv203a/Program.cs
@@ -0,0 +1,37 @@
+﻿using Course.Entities;
+using Course.Services;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental date");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy HH:mm: ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy HH:mm: ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            Console.Write("Enter price per hour: ");
+            double pricePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double pricePerday = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            RentalService rentalService = new RentalService(pricePerHour, pricePerday);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE:");
+            Console.WriteLine(carRental.Invoice);
+
+
+        }
+    }
+}
---
+++ Atv203a/Services/BrazilTaxService.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Course.Services
+{
+     class BrazilTaxService
+    {
+        public double Tax(double ammount)
+        {
+            if (ammount <= 100.0)
+            {
+                return ammount * 0.2;
+            }
+            else
+            {
+                return ammount * 0.15;
+            }
+        }
+    }
+}
